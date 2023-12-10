2dbce4ea1c8289e9a2a8275f2f4e46bb8ed4b849
Huang Zhiwen <hzw@MacBook-Pro-4.local>
2023-12-10 13:26:52 +0800

    minor

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -17,7 +17,8 @@ const Index = () => (
           </p> */}
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Welcome to my website. 
+        Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
